5eff235d21e81c7d4aecaf17fe32eb17c6f7c0e7
Bmannen <55086845+Bmannen@users.noreply.github.com>
2019-09-10 11:05:55 +0200

    MOVEMENT KLART

--- Loningsdagen/Assets/PlayerMovement.cs
+++ Loningsdagen/Assets/PlayerMovement.cs
@@ -6,15 +6,36 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public float rotSpeed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input
     }
 
     private void FixedUpdate()
     {
-        
+        //Movement
+        if(Input.GetKey(KeyCode.A)){
+            rb.rotation = rb.rotation + rotSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.rotation = rb.rotation - rotSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + transform.up * moveSpeed * Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + transform.up * -moveSpeed * Time.fixedDeltaTime;
+        }
+
+
     }
 }
