4248a043823be658121545a71a368623d6385328
Bmannen <55086845+Bmannen@users.noreply.github.com>
2019-09-10 10:32:19 +0200

    La till Movement Script

---
+++ Loningsdagen/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
