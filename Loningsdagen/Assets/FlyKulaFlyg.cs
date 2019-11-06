ffbd11d0da2f5748944b518daf0890680dcdee6d
Axel Johansson <01AXJ001@lulea.se>
2019-11-06 10:37:14 +0100

    Ove brappar

---
+++ Loningsdagen/Assets/FlyKulaFlyg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyKulaFlyg : MonoBehaviour
+{
+
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.up * Time.fixedDeltaTime * bulletSpeed;
+    }
+}
---
+++ Loningsdagen/Assets/Skjutande.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skjutande : MonoBehaviour
+{
+
+    public bool harPistol = true; //Ändra när han plockar upp pistol
+    public GameObject hylsa;
+    GameObject skott;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (harPistol)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log("Försöker skjuta");
+                skott = Instantiate(hylsa, transform.position , transform.rotation);
+
+            }
+        }
+    }
+}
