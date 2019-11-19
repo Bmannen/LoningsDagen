b0779803ba6beb822d3524334f2e5a11ed902b35
Axel Johansson <01AXJ001@lulea.se>
2019-11-19 10:13:47 +0100

    Bankdörr

---
+++ Loningsdagen/Assets/SesamOpenFastStörre.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SesamOpenFastStörre : MonoBehaviour
+{
+    public Collider2D door;
+    private bool näraDörr = false;
+    private bool dörrÖpen = false;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (näraDörr == true && Input.GetKeyDown(KeyCode.E) && dörrÖpen == false)
+        {
+            transform.Rotate(0, 0, -90);
+            transform.position = transform.position + transform.up * 2f + transform.right * -1f;
+            transform.Rotate(0, 0, 90);
+            dörrÖpen = true;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D door)
+    {
+        if (door.gameObject.name == "Player")
+        {
+            näraDörr = true;
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D door)
+    {
+
+    }
+
+    private void OnTriggerExit2D(Collider2D door)
+    {
+        näraDörr = false;
+    }
+}
